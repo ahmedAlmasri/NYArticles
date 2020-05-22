277dcdab031c441297d0ae153eda786e210abe40
Ahmad Almasri <ahmed.almasri@ymail.com>
2020-05-22 16:00:05 +0300

    init project

---
+++ NYArticles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYArticles
+//
+//  Created by Ahmad Almasri on 5/21/20.
+//  Copyright © 2020 Ahmad Almasri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
